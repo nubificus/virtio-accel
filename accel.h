f5661d3e6ecee50bdfbf0cce2ea091f164440c5d
Babis Chalios <mail@bchalios.io>
2020-11-06 04:34:12 -0600

    Cleanup module code
    Reorganize dir structure putting examples and tests in their own dir.
    Also cleanup code to increase code re-use and simplifying user API.
    
    Finally, add a github action to build the module and a vmlinux to use
    with firecracker
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- accel.h
+++ accel.h
@@ -7,58 +7,37 @@
 #endif
 
 /* IOCTLs */
-#define ACCIOC_GEN_SESS_CREATE      _IOWR('@', 0, struct accel_session)
-#define ACCIOC_GEN_SESS_DESTROY     _IOWR('@', 1, struct accel_session)
-#define ACCIOC_GEN_DO_OP            _IOWR('@', 2, struct accel_op)
-#define ACCIOC_CRYPTO_SESS_CREATE   _IOWR('@', 3, struct accel_session)
-#define ACCIOC_CRYPTO_SESS_DESTROY  _IOR('@', 4, struct accel_session)
-#define ACCIOC_CRYPTO_ENCRYPT       _IOWR('@', 5, struct accel_op)
-#define ACCIOC_CRYPTO_DECRYPT       _IOWR('@', 6, struct accel_op)
+#define VACCEL_SESS_CREATE      _IOWR('@', 0, struct accel_session)
+#define VACCEL_SESS_DESTROY     _IOWR('@', 1, struct accel_session)
+#define VACCEL_DO_OP            _IOWR('@', 2, struct accel_session)
 
 
-struct accel_crypto_sess {
-	__u32 cipher;
-	__u32 keylen;
-	__u8 __user *key;
-};
-
-struct accel_gen_op_arg {
+struct accel_arg {
 	__u32 len;
 	__u8 __user *buf;
 };
 
-struct accel_gen_op {
+struct accel_op {
+	/* number of in arguments */
 	__u32 in_nr;
+
+	/* pointer to in arguments */
+	struct accel_arg __user *in;
+
+	/* number of out arguments */
 	__u32 out_nr;
-	struct accel_gen_op_arg __user *in;
-	struct accel_gen_op_arg __user *out;
+
+	/* pointer to out arguments */
+	struct accel_arg __user *out;
 };
 
 struct accel_session {
+	/* Session id */
 	__u32 id;
-	union {
-		struct accel_crypto_sess crypto;
-		struct accel_gen_op gen;
-	} u;
-};
 
-struct accel_crypto_op {
-	__u32 src_len;
-	__u32 dst_len;
-	__u32 iv_len;
-	__u8 __user *src;
-	__u8 __user *dst;
-	__u8 __user *iv;
+	/* operation performed currently */
+	struct accel_op op;
 };
-
-struct accel_op {
-	__u32 session_id;
-	union {
-		struct accel_crypto_op crypto;
-		struct accel_gen_op gen;
-	} u;
-};
-
 
 int accel_dev_init(void);
 void accel_dev_destroy(void);
--- virtio_accel.h
+++ virtio_accel.h
@@ -15,29 +15,7 @@
 #define VIRTIO_ACCEL_NOTSUPP   3
 #define VIRTIO_ACCEL_INVSESS   4 /* Invalid session id */
 
-struct virtio_accel_crypto_sess {
-#define VIRTIO_ACCEL_C_NO_CIPHER      0
-#define VIRTIO_ACCEL_C_CIPHER_AES_ECB 1
-#define VIRTIO_ACCEL_C_CIPHER_AES_CBC 2
-#define VIRTIO_ACCEL_C_CIPHER_AES_CTR 3
-#define VIRTIO_ACCEL_C_CIPHER_AES_XTS 4
-	__virtio32 cipher;
-	__virtio32 keylen;
-	__u8 *key;
-	__u8 padding[7];
-};
-
-struct virtio_accel_crypto_op {
-	__virtio32 src_len;
-	__virtio32 dst_len;
-	__virtio32 iv_len;
-	__u8 *src;
-	__u8 *dst;
-	__u8 *iv;
-	__u8 padding;
-};
-
-struct virtio_accel_gen_op_arg {
+struct virtio_accel_arg {
 	__virtio32 len;
 	__u8 *buf;
 	__u8 __user *usr_buf;
@@ -46,48 +24,24 @@ struct virtio_accel_gen_op_arg {
 	__u8 padding[5];
 };
 
-struct virtio_accel_gen_op {
+struct virtio_accel_op {
 	__virtio32 in_nr;
 	__virtio32 out_nr;
-	struct virtio_accel_gen_op_arg *in;
-	struct virtio_accel_gen_op_arg *out;
+	struct virtio_accel_arg *in;
+	struct virtio_accel_arg *out;
 };
 
 struct virtio_accel_hdr {
-	__virtio32 session_id;
+	__virtio32 id;
 
-#define VIRTIO_ACCEL_NO_OP                        0
-#define VIRTIO_ACCEL_G_OP_CREATE_SESSION          1
-#define VIRTIO_ACCEL_G_OP_DESTROY_SESSION         2
-#define VIRTIO_ACCEL_G_OP_DO_OP                   3
-#define VIRTIO_ACCEL_C_OP_CIPHER_CREATE_SESSION   4
-#define VIRTIO_ACCEL_C_OP_CIPHER_DESTROY_SESSION  5
-#define VIRTIO_ACCEL_C_OP_CIPHER_ENCRYPT          6
-#define VIRTIO_ACCEL_C_OP_CIPHER_DECRYPT          7
-#define VIRTIO_ACCEL_C_OP_HASH_CREATE_SESSION     8
-#define VIRTIO_ACCEL_C_OP_MAC_CREATE_SESSION      9
-#define VIRTIO_ACCEL_C_OP_AEAD_CREATE_SESSION    10
-#define VIRTIO_ACCEL_C_OP_HASH_DESTROY_SESSION   11
-#define VIRTIO_ACCEL_C_OP_MAC_DESTROY_SESSION    12
-#define VIRTIO_ACCEL_C_OP_AEAD_DESTROY_SESSION   13
-#define VIRTIO_ACCEL_C_OP_HASH                   14
-#define VIRTIO_ACCEL_C_OP_MAC                    15
-#define VIRTIO_ACCEL_C_OP_AEAD_ENCRYPT           16
-#define VIRTIO_ACCEL_C_OP_AEAD_DECRYPT           17
-	__virtio32 op;
+#define VIRTIO_ACCEL_NO_OP                   0
+#define VIRTIO_ACCEL_CREATE_SESSION          1
+#define VIRTIO_ACCEL_DESTROY_SESSION         2
+#define VIRTIO_ACCEL_DO_OP                   3
+	__virtio32 op_type;
+
 	/* session create structs */
-	union {
-		struct virtio_accel_crypto_sess crypto_sess;
-		struct virtio_accel_crypto_op crypto_op;
-		struct virtio_accel_gen_op gen_op;
-	} u;
-};
-
-struct virtio_accel_crypto_conf {
-    /* Maximum length of cipher key */
-    __u32 max_cipher_key_len;
-    /* Maximum length of authenticated key */
-    __u32 max_auth_key_len;
+	struct virtio_accel_op op;
 };
 
 struct virtio_accel_conf {
@@ -96,10 +50,6 @@ struct virtio_accel_conf {
     __u32 services;
     /* Maximum size of each crypto request's content */
     __u64 max_size;
-
-    union {
-        struct virtio_accel_crypto_conf crypto;
-    } u;
 };
 
 #endif /* _VIRTIO_ACCEL_H */
