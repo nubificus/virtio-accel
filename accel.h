feaae5ec5bfc85dba9f48229b9f28657853bebd8
Kostis Papazafeiropoulos <papazof@gmail.com>
2023-02-10 00:10:43 +0200

    feat: Add virtio profiling implementation
    - Keep per session timers
    - Add a 'VACCEL_GET_TIMERS' ioctl to return virtio-accel timers
    - Return timers to vaccel similarly to the vsock plugin
    
    PR: https://github.com/nubificus/virtio-accel/pull/16
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- accel.h
+++ accel.h
@@ -6,10 +6,13 @@
 #define __user
 #endif
 
+#define TIMERS_NAME_MAX 64
+
 /* IOCTLs */
 #define VACCEL_SESS_CREATE      _IOWR('@', 0, struct accel_session)
 #define VACCEL_SESS_DESTROY     _IOWR('@', 1, struct accel_session)
 #define VACCEL_DO_OP            _IOWR('@', 2, struct accel_session)
+#define VACCEL_GET_TIMERS       _IOWR('@', 3, struct accel_session)
 
 
 struct accel_arg {
@@ -18,16 +21,16 @@ struct accel_arg {
 };
 
 struct accel_op {
-	/* number of in arguments */
+	/* Number of in arguments */
 	__u32 in_nr;
 
-	/* pointer to in arguments */
+	/* Pointer to in arguments */
 	struct accel_arg __user *in;
 
-	/* number of out arguments */
+	/* Number of out arguments */
 	__u32 out_nr;
 
-	/* pointer to out arguments */
+	/* Pointer to out arguments */
 	struct accel_arg __user *out;
 };
 
@@ -35,8 +38,30 @@ struct accel_session {
 	/* Session id */
 	__u32 id;
 
-	/* operation performed currently */
+	/* Operation performed currently */
 	struct accel_op op;
+};
+
+struct accel_prof_sample {
+	/* Timestamp (nsec) of entering the region */
+	__u64 start;
+
+	/* Time (nsec) elapsed inside the region */
+	__u64 time;
+};
+
+struct accel_prof_region {
+	/* Name of the region */
+	char name[TIMERS_NAME_MAX];
+
+	/* Number of collected samples */
+	__u64 nr_entries;
+
+	/* Array of collected samples */
+	struct accel_prof_sample *samples;
+
+	/* Allocated size for the array */
+	__u64 size;
 };
 
 int accel_dev_init(void);
---
+++ virtio_accel-session.c
@@ -0,0 +1,54 @@
+//#include <linux/uaccess.h>
+//#include <linux/string.h>
+//
+//#include "accel.h"
+
+#include <linux/slab.h>
+#include <linux/list.h>
+
+#include "virtio_accel-timers.h"
+
+
+struct virtio_accel_sess *virtaccel_session_create_and_add(
+		struct accel_session *accel_sess, struct virtio_accel_req *req)
+{
+	struct virtio_accel_sess *sess = kzalloc(sizeof(*sess), GFP_KERNEL);
+	if (sess) {
+		sess->id = accel_sess->id;
+		sess->nr_timers = 0;
+		virtaccel_timer_init(sess);
+		list_add_tail(&sess->node, &req->vaccel->sessions);
+	}
+
+	return sess;
+}
+
+void virtaccel_session_delete(struct accel_session *accel_sess,
+		struct virtio_accel_req *req)
+{
+	struct virtio_accel_sess *s = NULL, *tmp;
+
+	if (!accel_sess)
+		return;
+
+	list_for_each_entry_safe(s, tmp, &req->vaccel->sessions, node) {
+		if (s->id == accel_sess->id) {
+			list_del(&s->node);
+			virtaccel_timer_del_all(s);
+			kfree(s);
+		}
+	}
+}
+
+struct virtio_accel_sess *virtaccel_session_get_by_id(u32 id,
+		struct virtio_accel_req *req)
+{
+	struct virtio_accel_sess *s = NULL, *tmp;
+
+	list_for_each_entry_safe(s, tmp, &req->vaccel->sessions, node) {
+		if (s->id == id) {
+			return s;
+		}
+	}
+	return NULL;
+}
